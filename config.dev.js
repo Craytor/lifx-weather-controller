4e080b92cb20ce7fbb1e77c753047e8863e0d15f
Tyler Youschak <tyleryouschak@gmail.com>
2018-08-02 23:30:17 -0400

    Initial Commit

---
+++ config.dev.js
@@ -0,0 +1,19 @@
+module.exports = {
+  token: null,
+  lights: [
+    null
+  ],
+  lat: 37.4260,
+  lon: -79.1577,
+  colors: {
+    alerts: {
+      ffw: 'green',
+      tor: 'red',
+      tstrm: 'yellow'
+    },
+    default: {
+      color: 'kelvin:9000',
+      brightness: 1.0
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,85 @@
+var config = require('./config')
+var axios = require('axios')
+
+var lifxUrl = 'https://api.lifx.com/v1/lights'
+
+var determineEvent = null
+var updateRequired = false
+
+
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + config.token;
+
+
+getWeatherAlerts();
+setInterval(function() {getWeatherAlerts() }, 5000)
+
+function getWeatherAlerts() {
+  axios.get('https://api.weather.gov/alerts?active=true&point=' + config.lat + ',' + config.lon).then(function(response) {
+    var alerts = response.data.features;
+
+    for(var i=0; i < alerts.length;  i++) {
+
+      computeAlertEvent(alerts[i].properties.event)
+
+      if(i == (alerts.length - 1)) {
+        callLifxApi()
+      }
+
+    }
+
+  })
+}
+
+function computeAlertEvent(event) {
+  if(event == 'Tornado Warning') {
+    determineEvent = 'tor'
+    updateRequired = true;
+  } else if (event == 'Severe Thunderstorm Warning') {
+    if(determineEvent !== 'tor') {
+      determineEvent = 'tstrm'
+      updateRequired = true
+    }
+  } else if (event == 'Flash Flood Warning') {
+    if(determineEvent !== 'tstrm') {
+      determineEvent = 'ffw'
+      updateRequired = true
+    }
+  } else {
+    determineEvent = null
+  }
+}
+
+function callLifxApi() {
+  console.log('calling')
+  if(determineEvent == null) {
+    if(updateRequired) {
+      setLightsToDefaultState()
+      updateRequired = false;
+    }
+  } else {
+    console.log('updating lights')
+    axios.put(lifxUrl + '/all/state', {
+      power: 'on',
+      fast: true,
+      color: config.colors.alerts[determineEvent],
+      brightness: config.colors.default.brightness,
+    }).catch(function(response)  {
+      // error turing lights to default state
+      console.log(response)
+    })
+  }
+
+}
+
+function setLightsToDefaultState() {
+  console.log('setting to default')
+  axios.put(lifxUrl + '/all/state', {
+    power: 'on',
+    fast: true,
+    color: config.colors.default.color,
+    brightness: config.colors.default.brightness,
+  }).catch(function(response)  {
+    // error turing lights to default state
+    console.log(response)
+  })
+}
